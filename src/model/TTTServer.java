a36c9472b7fb7da21a64d4432878c64fdfa3518a
Alex <heimann.alexander@gmx.net>
2018-09-30 19:56:31 +0200

    Client verarbeitet alles Messages

--- src/model/TTTServer.java
+++ src/model/TTTServer.java
@@ -51,37 +51,40 @@ public class TTTServer extends Server {
     @Override
     public void processMessage(String pClientIP, int pClientPort, String pMessage) {
         String[] nachrichtenTeil = pMessage.split(";");
-        if(nachrichtenTeil[0].equals("CONNECT")){
-            processNewConnection(pClientIP, pClientPort);
-
-        } else if(nachrichtenTeil[0].equals("PICK")){
-            int x = Integer.parseInt(nachrichtenTeil[1]);
-            int y = Integer.parseInt(nachrichtenTeil[2]);
-            if (playerOnesTurn) {
-                send(playerIps[1], playerPorts[1], "OPPONENTPICK;" + x + ";" + y);
-                playerOnesTurn = false;
-            }else{
-                send(playerIps[0], playerPorts[0], "OPPONENTPICK;" + x + ";" + y);
-                playerOnesTurn = true;
-            }
-
-        } else if(nachrichtenTeil[0].equals("LEAVE")) {
-            processClosingConnection(pClientIP, pClientPort);
-
-        }else if(nachrichtenTeil[0].equals("WIN")) {
-            if (playerIps[0].equals(pClientIP)) {
-                send(playerIps[0], playerPorts[0], "WIN;Glückwunsch, du hast gewonnen!");
-                send(playerIps[1], playerPorts[1], "LOSE;Du hast leider verloren...");
-            } else {
-                send(playerIps[1], playerPorts[1], "WIN;Glückwunsch, du hast gewonnen!");
-                send(playerIps[0], playerPorts[0], "LOSE;Du hast leider verloren...");
-            }
-        }else if(nachrichtenTeil[0].equals("DRAW")){
-            send(playerIps[0], playerPorts[0], "DRAW;Unentschieden");
-            send(playerIps[1], playerPorts[1], "DRAW;Unentschieden");
-
-        }else{
-            send(pClientIP, pClientPort, "FALSECOMMAND;Geben Sie bitte einen richtigen Befehl ein.");
+        switch(nachrichtenTeil[0]){
+            case "CONNECT":
+                processNewConnection(pClientIP, pClientPort);
+                break;
+            case "PICK":
+                int x = Integer.parseInt(nachrichtenTeil[1]);
+                int y = Integer.parseInt(nachrichtenTeil[2]);
+                if (playerOnesTurn) {
+                    send(playerIps[1], playerPorts[1], "OPPONENTPICK;" + x + ";" + y);
+                    playerOnesTurn = false;
+                }else{
+                    send(playerIps[0], playerPorts[0], "OPPONENTPICK;" + x + ";" + y);
+                    playerOnesTurn = true;
+                }
+                break;
+            case "LEAVE":
+                processClosingConnection(pClientIP, pClientPort);
+                break;
+            case "WIN":
+                if (playerIps[0].equals(pClientIP)) {
+                    send(playerIps[0], playerPorts[0], "WIN;Glückwunsch, du hast gewonnen!");
+                    send(playerIps[1], playerPorts[1], "LOSE;Du hast leider verloren...");
+                } else {
+                    send(playerIps[1], playerPorts[1], "WIN;Glückwunsch, du hast gewonnen!");
+                    send(playerIps[0], playerPorts[0], "LOSE;Du hast leider verloren...");
+                }
+                break;
+            case "DRAW":
+                send(playerIps[0], playerPorts[0], "DRAW;Unentschieden");
+                send(playerIps[1], playerPorts[1], "DRAW;Unentschieden");
+                break;
+            default:
+                send(pClientIP, pClientPort, "FALSECOMMAND;Geben Sie bitte einen richtigen Befehl ein.");
+                break;
         }
     }
 
